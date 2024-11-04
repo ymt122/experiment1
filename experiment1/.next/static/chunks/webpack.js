5acc76ad918f5e0b5f490287ee2c78ba505c08f0
ymt122 <ymtychr122@gmail.com>
2024-11-04 16:38:31 +0900

    add mongodb dependency

--- experiment1/.next/static/chunks/webpack.js
+++ experiment1/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a859a1d0d90871d6"; }
+/******/ 		__webpack_require__.h = function() { return "7acaadea135dc16e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
